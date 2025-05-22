e86e1d338fe221abf5738dca068bcc2f069b0548
Mere Solace <btm7428@gmail.com>
2025-05-22 09:54:35 -0400

    Initial commit

---
+++ src/main/java/btm/sword/Sword.java
@@ -0,0 +1,18 @@
+package btm.sword;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Sword extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+		
+	}
+}
