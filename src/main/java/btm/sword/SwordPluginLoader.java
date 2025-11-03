9629f57c224eb7f7764614e02ba60e8d49db8f63
Mere-Solace <btm7428@gmail.com>
2025-11-03 13:26:41 -0500

    Removed commented out code

--- src/main/java/btm/sword/SwordPluginLoader.java
+++ src/main/java/btm/sword/SwordPluginLoader.java
@@ -2,9 +2,7 @@ package btm.sword;
 
 import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
 import io.papermc.paper.plugin.loader.PluginLoader;
-import io.papermc.paper.plugin.loader.library.impl.JarLibrary;
 import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
-import java.nio.file.Path;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.repository.RemoteRepository;
@@ -18,24 +16,4 @@ public class SwordPluginLoader implements PluginLoader {
         resolver.addDependency(new Dependency(new DefaultArtifact("xyz.xenondevs.invui:invui:pom:1.47"), null));
         pluginClasspathBuilder.addLibrary(resolver);
     }
-
-//    @Override
-//    public void classloader(PluginClasspathBuilder classpathBuilder) {
-//        classpathBuilder.addLibrary(new JarLibrary(Path.of("dependency.jar")));
-//
-//        MavenLibraryResolver resolver = new MavenLibraryResolver();
-//
-//        resolver.addRepository(new RemoteRepository.Builder(
-//                "xenondevs",
-//                "default",
-//                "https://repo.xenondevs.xyz/releases/")
-//                .build());
-//
-//        resolver.addDependency(new Dependency(
-//                new DefaultArtifact("xyz.xenondevs.invui:invui:1.0"),
-//                null
-//        ));
-//
-//        classpathBuilder.addLibrary(resolver);
-//    }
 }
