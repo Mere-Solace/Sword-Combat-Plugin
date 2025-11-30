23474005ab4bae397d30235271209d0bdfaaac78
Rounak Shrivastav <shrivastavrounak6@gmail.com>
2025-11-30 11:41:43 +0530

    Improved code readability in MovementAction.java

--- src/main/java/btm/sword/system/action/MovementAction.java
+++ src/main/java/btm/sword/system/action/MovementAction.java
@@ -10,9 +10,9 @@ import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ItemDisplay;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -53,125 +53,240 @@ public class MovementAction extends SwordAction {
      */ // TODO: #125 - This method is illegible, needs refactoring for readability
     public static void dash(Combatant executor, boolean forward) {
         double maxDistance = Config.Movement.DASH_MAX_DISTANCE;
-        cast (executor, Config.Movement.DASH_CAST_DURATION, new BukkitRunnable() {
+        cast(executor, Config.Movement.DASH_CAST_DURATION, new BukkitRunnable() {
             @Override
             public void run() {
                 LivingEntity ex = executor.self();
+
+                // Starting point of dash effects
                 final Location dashStartLocation = ex.getLocation().add(new Vector(0, Config.Movement.DASH_INITIAL_OFFSET_Y, 0));
                 boolean onGround = executor.isGrounded();
                 Location o = ex.getEyeLocation();
 
-                // for use in dash attack method.
-                executor.setDashDirection(ex.getEyeLocation().getDirection().multiply(forward ? 1 : -1));
-
+                // Store initial direction for follow-up dash attack logic
+                Vector dashDir = o.getDirection().multiply(forward ? 1 : -1);
+                executor.setDashDirection(dashDir);
+                // Apply dash speed
                 PotionEffect speed = new PotionEffect(PotionEffectType.SPEED, Config.Movement.SPEED_DURATION, Config.Movement.SPEED_AMPLIFIER);
                 ex.addPotionEffect(speed);
 
-                // check for an item that may be the target of the dash
-                Entity targetedItem;
+                // Determine if the dash has a target item
+                Entity targetItem = findTargetItem(executor, o, maxDistance);
 
-                if (executor.getItemStackInHand(true).isEmpty()) {
-                    targetedItem = HitboxUtil.ray(o, o.getDirection(), maxDistance, Config.Movement.DASH_RAY_HITBOX_RADIUS,
-                        entity -> (entity.getType() == EntityType.ITEM_DISPLAY &&
-                            !entity.isDead() &&
-                            entity instanceof ItemDisplay id &&
-                            InteractiveItemArbiter.checkIfInteractive(id)) &&
-                            !InteractiveItemArbiter.isImpaling(SwordEntityArbiter.get(ex.getUniqueId()), id));
-                } else {
-                    targetedItem = null; //  can't dash and grab a new item off the ground if already holding something
-                }
+                if (isValidDisplayItem(targetItem)) {
 
-                if (targetedItem instanceof ItemDisplay id &&
-                        !id.isDead() &&
-                        !id.getItemStack().isEmpty()) {
-                    RayTraceResult impedanceCheck = ex.getWorld().rayTraceBlocks(
-                            ex.getLocation().add(new Vector(0, Config.Movement.DASH_IMPEDANCE_CHECK_OFFSET_Y, 0)),
-                            targetedItem.getLocation().subtract(ex.getLocation()).toVector().normalize(),
-                            maxDistance/2, FluidCollisionMode.NEVER,
-                            true,
-                            block -> !block.isCollidable());
+                    ItemDisplay display = (ItemDisplay) targetItem;
 
-                    new BukkitRunnable() {
-                        int t = 0;
-                        @Override
-                        public void run() {
-                            DrawUtil.secant(List.of(Prefab.Particles.TEST_SWORD_BLUE), dashStartLocation, ex.getLocation(), Config.Movement.DASH_SECANT_RADIUS);
-                            t += Config.Movement.DASH_PARTICLE_TIMER_INCREMENT;
-                            if (t > Config.Movement.DASH_PARTICLE_TIMER_THRESHOLD) cancel();
-                        }
-                    }.runTaskTimer(Sword.getInstance(), Config.Movement.DASH_PARTICLE_TASK_DELAY, Config.Movement.DASH_PARTICLE_TASK_PERIOD);
+                    // Check if blocks obstruct the dash path
+                    boolean blocked = hasObstruction(ex, display, maxDistance);
 
-                    if (impedanceCheck == null || impedanceCheck.getHitBlock() == null) {
-                        double length = id.getLocation().subtract(ex.getEyeLocation()).length();
+                    startDashParticles(dashStartLocation, ex);
 
-                        executor.setVelocity(ex.getEyeLocation().getDirection().multiply(Math.log(length)));
-
-                        Vector u = executor.getFlatDir().multiply(forward ? Config.Movement.DASH_FORWARD_MULTIPLIER : -Config.Movement.DASH_FORWARD_MULTIPLIER)
-                                .add(Config.Direction.UP().multiply(Config.Movement.DASH_UPWARD_MULTIPLIER));
-
-                        new BukkitRunnable() {
-                            @Override
-                            public void run() {
-                                if (id.getLocation().subtract(ex.getEyeLocation()).lengthSquared() < Config.Movement.DASH_GRAB_DISTANCE_SQUARED) {
-                                    BlockData blockData = ex.getLocation().add(new Vector(0, Config.Movement.DASH_BLOCK_CHECK_OFFSET_Y, 0)).getBlock().getBlockData();
-                                    new ParticleWrapper(Particle.BLOCK,
-                                            Config.Movement.DASH_PARTICLE_COUNT,
-                                            Config.Movement.DASH_PARTICLE_SPREAD_X,
-                                            Config.Movement.DASH_PARTICLE_SPREAD_Y,
-                                            Config.Movement.DASH_PARTICLE_SPREAD_Z,
-                                            blockData).display(ex.getLocation());
-                                    SoundUtil.playSound(ex, SoundType.ENTITY_ENDER_DRAGON_FLAP, Config.Movement.DASH_FLAP_SOUND_VOLUME, Config.Movement.DASH_FLAP_SOUND_PITCH);
-                                    SoundUtil.playSound(ex, SoundType.ENTITY_PLAYER_ATTACK_SWEEP, Config.Movement.DASH_SWEEP_SOUND_VOLUME, Config.Movement.DASH_SWEEP_SOUND_PITCH);
-                                    executor.setVelocity(u);
-
-                                    InteractiveItemArbiter.onGrab(id, executor); // here is where the display is taken care of
-                                }
-                                else {
-                                    Vector v = ex.getVelocity();
-                                    double damping = Config.Movement.DASH_VELOCITY_DAMPING;
-                                    ex.setVelocity(new Vector(v.getX() * damping, v.getY() * damping, v.getZ() * damping));
-                                    executor.message("Didn't get there");
-                                }
-                            }
-                        }.runTaskLater(Sword.getInstance(), Config.Movement.DASH_GRAB_CHECK_DELAY);
+                    if (!blocked) {
+                        handleTargetedDash(executor, ex, display, forward);
                         return;
                     }
-                    else {
-                        executor.message("You can't dash to that item...");
-                    }
+
+                    executor.message("You can't dash to that item...");
                 }
 
-
-                double dashPower = Config.Movement.DASH_BASE_POWER;
-                double s = forward ? dashPower : -dashPower;
-                Vector up = Config.Direction.UP().multiply(Config.Movement.DASH_UPWARD_BOOST);
-                new BukkitRunnable() {
-                    int i = 0;
-                    @Override
-                    public void run() {
-                        Vector dir = ex.getEyeLocation().getDirection();
-                        if (onGround && (
-                                (forward && dir.dot(new Vector(0, 1, 0)) < 0)
-                                        ||
-                                        (!forward && dir.dot(new Vector(0, 1, 0)) > 0))) {
-                            dir = executor.getFlatDir();
-                        }
-                        if (i == 0)
-                            ex.setVelocity(dir.multiply(s).add(up));
-                        else if (i == 1) {
-                            ex.setVelocity(dir.multiply(s));
-                        }
-                        else {
-                            cancel();
-                        }
-                        i++;
-                    }
-                }.runTaskTimer(Sword.getInstance(), Config.Movement.DASH_VELOCITY_TASK_DELAY, Config.Movement.DASH_VELOCITY_TASK_PERIOD);
-                if (!onGround)
-                    executor.increaseAirDashesPerformed();
+                // Default untargeted dash
+                performNormalDash(executor, ex, onGround, forward);
             }
         });
     }
+
+
+private static Entity findTargetItem(Combatant executor, Location eye, double maxDistance) {
+
+    if (!executor.getItemStackInHand(true).isEmpty()) {
+        return null;
+    }
+
+    return HitboxUtil.ray(
+            eye,
+            eye.getDirection(),
+            maxDistance,
+            Config.Movement.DASH_RAY_HITBOX_RADIUS,
+            entity -> entity instanceof ItemDisplay id &&
+                      !entity.isDead() &&
+                      !InteractiveItemArbiter.isImpaling(
+                          SwordEntityArbiter.get(executor.self().getUniqueId()), id) &&
+                      InteractiveItemArbiter.checkIfInteractive(id));
+}
+
+private static boolean isValidDisplayItem(Entity e) {
+    return e instanceof ItemDisplay id &&
+           !id.isDead() &&
+           !id.getItemStack().isEmpty();
+}
+
+private static boolean hasObstruction(LivingEntity ex, ItemDisplay target, double maxDistance) {
+
+    // Where to begin checking for a block that might interrupt the dash
+    Location start = ex.getLocation().add(
+            0,
+            Config.Movement.DASH_IMPEDANCE_CHECK_OFFSET_Y,
+            0
+    );
+
+    // Direction from player → target
+    Vector dir = target.getLocation().subtract(ex.getLocation()).toVector().normalize();
+
+    RayTraceResult result = ex.getWorld().rayTraceBlocks(
+            start,
+            dir,
+            maxDistance / 2,
+            FluidCollisionMode.NEVER,
+            true,
+            block -> !block.isCollidable()
+    );
+
+    return result != null && result.getHitBlock() != null;
+}
+
+private static void startDashParticles(Location dashStartLocation, LivingEntity ex) {
+
+    new BukkitRunnable() {
+        int t = 0;
+
+        @Override
+        public void run() {
+            DrawUtil.secant(
+                    List.of(Prefab.Particles.TEST_SWORD_BLUE),
+                    dashStartLocation,
+                    ex.getLocation(),
+                    Config.Movement.DASH_SECANT_RADIUS
+            );
+
+            t += Config.Movement.DASH_PARTICLE_TIMER_INCREMENT;
+            if (t > Config.Movement.DASH_PARTICLE_TIMER_THRESHOLD) {
+                cancel();
+            }
+        }
+    }.runTaskTimer(
+            Sword.getInstance(),
+            Config.Movement.DASH_PARTICLE_TASK_DELAY,
+            Config.Movement.DASH_PARTICLE_TASK_PERIOD
+    );
+}
+
+private static void handleTargetedDash(Combatant executor, LivingEntity ex, ItemDisplay target, boolean forward) {
+
+    double distance = target.getLocation().subtract(ex.getEyeLocation()).length();
+
+    // Initial velocity toward target based on distance
+    executor.setVelocity(ex.getEyeLocation().getDirection().multiply(Math.log(distance)));
+
+    // Up + forward velocity used upon successful grab
+    Vector postGrabVelocity = executor.getFlatDir()
+            .multiply(forward ? Config.Movement.DASH_FORWARD_MULTIPLIER : -Config.Movement.DASH_FORWARD_MULTIPLIER)
+            .add(Config.Direction.UP().multiply(Config.Movement.DASH_UPWARD_MULTIPLIER));
+
+    new BukkitRunnable() {
+        @Override
+        public void run() {
+
+            double distSq = target.getLocation().subtract(ex.getEyeLocation()).lengthSquared();
+
+            if (distSq < Config.Movement.DASH_GRAB_DISTANCE_SQUARED) {
+                handleSuccessfulGrab(executor, ex, target, postGrabVelocity);
+            } else {
+                handleFailedGrab(ex);
+            }
+        }
+    }.runTaskLater(Sword.getInstance(), Config.Movement.DASH_GRAB_CHECK_DELAY);
+}
+
+private static void handleSuccessfulGrab(Combatant executor, LivingEntity ex, ItemDisplay target, Vector velocity) {
+
+    BlockData blockData = ex.getLocation()
+            .add(0, Config.Movement.DASH_BLOCK_CHECK_OFFSET_Y, 0)
+            .getBlock()
+            .getBlockData();
+
+    new ParticleWrapper(
+            Particle.BLOCK,
+            Config.Movement.DASH_PARTICLE_COUNT,
+            Config.Movement.DASH_PARTICLE_SPREAD_X,
+            Config.Movement.DASH_PARTICLE_SPREAD_Y,
+            Config.Movement.DASH_PARTICLE_SPREAD_Z,
+            blockData
+    ).display(ex.getLocation());
+
+    SoundUtil.playSound(ex, SoundType.ENTITY_ENDER_DRAGON_FLAP,
+            Config.Movement.DASH_FLAP_SOUND_VOLUME,
+            Config.Movement.DASH_FLAP_SOUND_PITCH);
+
+    SoundUtil.playSound(ex, SoundType.ENTITY_PLAYER_ATTACK_SWEEP,
+            Config.Movement.DASH_SWEEP_SOUND_VOLUME,
+            Config.Movement.DASH_SWEEP_SOUND_PITCH);
+
+    executor.setVelocity(velocity);
+
+    // Perform the actual “item pickup / grab”
+    InteractiveItemArbiter.onGrab(target, executor);
+}
+
+private static void handleFailedGrab(LivingEntity ex) {
+
+    Vector vel = ex.getVelocity();
+    double damping = Config.Movement.DASH_VELOCITY_DAMPING;
+
+    ex.setVelocity(new Vector(
+            vel.getX() * damping,
+            vel.getY() * damping,
+            vel.getZ() * damping
+    ));
+
+    if (ex instanceof Player p) {
+        p.sendMessage("Didn't get there");
+    }
+}
+
+private static void performNormalDash(Combatant executor, LivingEntity ex, boolean grounded, boolean forward) {
+
+    double base = Config.Movement.DASH_BASE_POWER;
+    double power = forward ? base : -base;
+
+    Vector upBoost = Config.Direction.UP().multiply(Config.Movement.DASH_UPWARD_BOOST);
+
+    new BukkitRunnable() {
+        int i = 0;
+
+        @Override
+        public void run() {
+
+            Vector dir = ex.getEyeLocation().getDirection();
+
+            if (grounded && (
+                (forward && dir.dot(new Vector(0, 1, 0)) < 0) ||
+                (!forward && dir.dot(new Vector(0, 1, 0)) > 0)
+            )) {
+                dir = executor.getFlatDir();
+            }
+
+            if (i == 0) {
+                ex.setVelocity(dir.multiply(power).add(upBoost));
+            } else if (i == 1) {
+                ex.setVelocity(dir.multiply(power));
+            } else {
+                cancel();
+            }
+
+            i++;
+        }
+    }.runTaskTimer(
+            Sword.getInstance(),
+            Config.Movement.DASH_VELOCITY_TASK_DELAY,
+            Config.Movement.DASH_VELOCITY_TASK_PERIOD
+    );
+
+    if (!grounded) {
+        executor.increaseAirDashesPerformed();
+    }
+}
+
 
     /**
      * Tosses the specified {@link SwordEntity} away from the executor.
